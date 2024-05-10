36658f208dc792ed2842d1a7c27a0b13120e98cb
sageMode <nathanielmvana@gmail.com>
2024-05-10 16:54:51 +0200

    Added dummy data

---
+++ script.js
@@ -0,0 +1,44 @@
+//Dummpy data
+const properties = [
+    {
+      id: 1,
+      name: 'Luxury Apartment',
+      location: 'Cape Town',
+      rent: 8000,
+      area: 1200,
+      images: ['https://example.com/image1.jpg', 'https://example.com/image2.jpg'],
+      agentName: 'John Smith',
+      agentEmail: 'agent1@example.com'
+    },
+    {
+      id: 2,
+      name: 'The Loft Life',
+      location: 'observatory',
+      rent: 4500,
+      area: 900,
+      images: ['https://example.com/image3.jpg'],
+      agentName: 'John Smith',
+      agentEmail: 'agent2@example.com'
+    },
+    {
+        id: 3,
+        name: 'The Urban Retreat',
+        location: 'Milnerton',
+        rent: 7000,
+        area: 1000,
+        images: ['https://example.com/image3.jpg'],
+        agentName: 'GI Joe',
+        agentEmail: 'agent2@example.com'
+      },
+      {
+        id: 4,
+        name: 'The Elevated Abode',
+        location: 'Khayelitsha',
+        rent: 5000,
+        area: 900,
+        images: ['https://example.com/image3.jpg'],
+        agentName: 'GI Joe',
+        agentEmail: 'agent2@example.com'
+      }
+  ];
+
